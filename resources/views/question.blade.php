826f1654b0cdedcd7a24d7adbdd979d0696458c6
yapancha <YoshiPanchu1995>
2018-11-21 21:45:20 -0500

    Feature: Edit/delete the answer to the question

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -39,7 +39,7 @@
                                 <div class="card-footer">
 
                                     <a class="btn btn-primary float-right"
-                                       href="{{ route('answer.show', ['question_id'=> $question->id,'answer_id' => $answer->id]) }}">
+                                       href="{{ route('answers.show', ['question_id'=> $question->id,'answer_id' => $answer->id]) }}">
                                         View
                                     </a>
 
