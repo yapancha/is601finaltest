1ae40b6c495a3752a7172d004a8c4c067028c4a0
yapancha <YoshiPanchu1995>
2018-12-13 22:10:16 -0500

    Feature: Comment display under each question

--- resources/views/answer.blade.php
+++ resources/views/answer.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Answer</div>
+                    <div class="card-header "><h3>Answer</h3></div>
                     <div class="card-body">
                         {{$answer->body}}
                     </div>
@@ -21,7 +21,30 @@
                         </a>
                     </div>
                     @endif
-                    {{$answer->comments}}
+
+                    <div class="card">
+                        <div class="card-header"><strong>Comments</strong></div>
+
+
+                    @forelse($answer->comments as $comment)
+                        <div class="card-header">
+                            <div class="card-body text-muted">
+                                {{$comment->body}}
+                                {{--@if(Auth::user()->id === $comment->user->id)--}}
+                                    {{ Form::open(['method'  => 'DELETE', 'route' => ['answers.destroy', $question, $answer->id]])}}
+                                    <button class="btn btn-danger float-right mr-2" value="submit" type="submit" id="submit">Delete
+                                    </button>
+                                    {!! Form::close() !!}
+                                    <a class="btn btn-primary float-right" href="{{ route('answers.edit',['question_id'=> $question, 'answer_id'=> $answer->id, ])}}">
+                                        Edit
+                                    </a>
+                                {{--@endif--}}
+                            </div>
+                        </div>
+                    @empty
+                        No comments.
+                    @endforelse
+                    </div>
                 </div>
 
             </div>
